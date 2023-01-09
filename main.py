ccb52ec50d1e498d8cc9682e40f915fa6b912a01
Philipp Schütz <mail@philippschuetz.de>
2023-01-09 16:53:22 +0100

    Add data validation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webbrowser
 import os
 import templates
 from datetime import date
+from datetime import datetime
 import time
 from typing import List
 
@@ -9,25 +10,44 @@ from typing import List
 
 class App():
     def __init__(self) -> None:
-        self.current_date = date.today().strftime("%d.%m.%Y")
+        self.current_date = date.today().strftime('%d.%m.%Y')
 
     def open_web(self):
         'open overview todays birthdays in browser'
         webbrowser.open('file://' + os.path.realpath('birthday.html'))
 
     def read_file(self) -> List[list]:
-        return_list = []
+        persons = []
         with open('input.csv', 'r') as file:
             for line in file:
                 tmp = line.split(',')
                 if ' \n' in tmp:
                     tmp.remove(' \n')
                 if tmp[2] == self.current_date:
-                    return_list.append(tmp)
-        return return_list
+                    persons.append(tmp)
+        return persons
 
-    def validate_input(self):
-        pass
+    def validate_date(date:str):
+        try:
+            datetime.strptime(date, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError('date should be formatted like dd.mm.yyyy')
+        return True
+  
+    def validate_gender(gender:str):
+        """checks if gender is one of the three allowed"""
+        genders = ['m','w','n']
+        if gender in genders:
+            return True
+        else:
+            raise ValueError('gender should be m, w, or n')
+
+    def validate_video(video:str):
+        """checks if video has format mp4"""
+        if video.endswith('.mp4'):
+            return True
+        else:
+            raise ValueError('videos should have type mp4')
 
     def prepare(self, persons: List[list]) -> List[dict]:
         'prepare data for displaying'
@@ -36,14 +56,17 @@ class App():
             tmp_dict = {}
             tmp_dict['firstname'] = person[1]
             tmp_dict['lastname'] = person[0]
-            tmp_dict['gender'] = person[3]
-            tmp_dict['birthdate'] = person[2]
+            if self.validate_gender(person[3]):
+                tmp_dict['gender'] = person[3]
+            if self.validate_date(person[2]):
+                tmp_dict['birthdate'] = person[2]
 
             if len(person) > 4:
                 if person[4].endswith('.mp4'):
                     tmp_dict['video'] = person[4]
                 else:
-                    tmp_dict['image'] = person[4]
+                    if self.validate_video(person[4]):
+                        tmp_dict['image'] = person[4]
             if len(person) > 5:
                 tmp_dict['video'] = person[5]
             
