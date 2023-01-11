d14daa442386880f2169c8ab645bc17be97c9ef5
Philipp Schütz <mail@philippschuetz.de>
2023-01-11 13:38:58 +0100

    Add Documentation

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # birtday calendar
 
+## how it works
+
 - displays information about a person whose birthday is today, on a browser page
-- the page refreshes at 00:05 every day
+- the elements used for the page generation are defined in templates.py
+- the browser page is generated at launch of main.py and on a new day
+- the browser page refreshes at 00:05 every day
 - setup.py puts this program into windows autostart
+
+## input data
+
+- input data must be in a .csv file formatted like example.csv at the root of the project
+- videos can only be in .mp4 format
+- files referenced in the input file must be at the root of the project
+- values lastname, firstname, birthdate and gender are required
--- main.py
+++ main.py
@@ -6,28 +6,30 @@ from datetime import datetime
 import time
 from typing import List
 
-# only videos in mp4 format
+# TODO: only days and months of the dates
+# TODO: check if files (savefile, image) exist in folder
 
 class App():
     def __init__(self) -> None:
         self.current_date = date.today().strftime('%d.%m.%Y')
 
     def open_web(self):
-        'open overview todays birthdays in browser'
+        """open overview todays birthdays in browser"""
         webbrowser.open('file://' + os.path.realpath('birthday.html'))
 
     def read_file(self) -> List[list]:
+        """read data from file, remove unnecessary characters and convert to 2d list"""
         persons = []
-        with open('input_2.csv', 'r') as file:
+        with open('input.csv', 'r') as file:
             for line in file:
                 line = line.strip().strip(',')
-
                 tmp = line.split(',')
                 if tmp[2] == self.current_date:
                     persons.append(tmp)
         return persons
 
     def validate_date(self, date:str):
+        """validate datestring from input file"""
         try:
             datetime.strptime(date, '%d.%m.%Y')
         except ValueError:
@@ -42,16 +44,10 @@ class App():
         else:
             raise ValueError('gender should be m, w, or n')
 
-    def validate_video(self, video:str):
-        """checks if video has format mp4"""
-        if video.endswith('.mp4'):
-            return True
-        else:
-            raise ValueError('videos should have type mp4')
-
     def prepare(self, persons: List[list]) -> List[dict]:
-        'prepare data for displaying'
+        """prepare data for displaying"""
         return_list = []
+        # loop through persons and append sorted data to a dictionary
         for person in persons:
             tmp_dict = {}
             tmp_dict['firstname'] = person[1]
@@ -66,7 +62,7 @@ class App():
                 if person[4].endswith('.mp4'):
                     tmp_dict['video'] = person[4]
                 else:
-                    if person[4].endswith('.png'): # TODO: validate img method
+                    if person[4].endswith('.png'):
                         tmp_dict['image'] = person[4]
             if len(person) > 5:
                 tmp_dict['video'] = person[5]
@@ -76,8 +72,9 @@ class App():
         return return_list
 
     def generate_page(self, persons: List[dict]):
-        'generate web page for displaying birthdays'
+        """generate web page for displaying birthdays"""
         title = 'Birthday'
+        # write modified templates to html file
         with open('birthday.html', 'w') as file:
             file.write(templates.top.format(title=title))
             # check if there is a birthday
@@ -92,24 +89,27 @@ class App():
                         file.write(templates.video.format(source=person['video']))
                     file.write(templates.person_close)
             else:
+                # write templates for the case that no birthday exists on the current date
                 file.write(templates.person_open)
                 file.write(templates.no_bd)
                 file.write(templates.person_close)
             file.write(templates.bottom)
 
     def run(self):
-        # open page on startup, realoads later with js
+        """run function of the project"""
         self.generate_page(self.prepare(self.read_file()))
         self.open_web()
         while True:
+            # check if the current date is a new date
             tmp = date.today().strftime("%d.%m.%Y")
             if tmp != self.current_date:
                 self.current_date = tmp
-                # rerun on new date
+                # generate page on new date
                 self.generate_page(self.prepare(self.read_file()))
                 self.open_web()
+            #wait 1 minute
             time.sleep(60)
 
 if __name__ == '__main__':
-    app = App()
-    app.run()
+    # run app
+    App().run()
