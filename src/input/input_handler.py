5161f492fcec733f12984453d645181a4999d02f
philipp-schuetz <mail@philippschuetz.com>
2024-02-01 18:00:16 +0100

    Add new files + ideas

---
+++ src/input/input_handler.py
@@ -0,0 +1,10 @@
+"""modules receives an input method and a filename and returns the validated input data if it exists"""
+
+# is it maybe possible to implement the different versions of input reading with oop interfaces,
+# what would be the most pythonic way to do this
+# oop is an interesting thing in general, when it comes to developing in python
+#
+# i it is possible for me to do this usig interfaces, I could to one definition and implement this for each input method
+# the input method/implementation to use is handled and managed by the input_handler.py
+# data validation is already with the person type
+# can I enforce data and throw errors, if there is no data?
