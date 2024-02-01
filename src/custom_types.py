6eff395ab092439d5cf26ecac31d40e27b121ade
philipp-schuetz <mail@philippschuetz.com>
2024-02-01 20:57:21 +0100

    Add file path validation and getters on person custom type

--- src/custom_types.py
+++ src/custom_types.py
@@ -11,7 +11,32 @@ class Person:
         if len(gender) != 1 or gender not in ['m', 'f', 'n']:
             raise ValueError("gender must be one of the following: m, f, n")
         else:
-            self.gender = gender.lower()
+            self.gender = gender
         self.birthdate = birthdate
+
+        # if a file path was provided, check if the file exists
+        if image is not None and not image.exists():
+            raise ValueError(f'file {image} does not exist')
         self.image = image
+
+        if video is not None and not video.exists():
+            raise ValueError(f'file {video} does not exist')
         self.video = video
+
+    def get_firstname(self) -> str:
+        return self.firstname
+
+    def get_lastname(self) -> str:
+        return self.lastname
+
+    def get_gender(self) -> str:
+        return self.gender
+
+    def get_birthdate(self) -> dt.date:
+        return self.birthdate
+
+    def get_image(self) -> Path | None:
+        return self.image
+
+    def get_video(self) -> Path | None:
+        return self.video
